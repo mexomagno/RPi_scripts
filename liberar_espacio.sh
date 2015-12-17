04cd54a7665f3707fc40eb344a9a0e497cb1fa37
Maximiliano Castro <macguionbajo@gmail.com>
2015-12-17 15:28:26 -0300

    Mejorado borrado y agregado espacio liberado human readable

--- liberar_espacio.sh
+++ liberar_espacio.sh
@@ -22,6 +22,9 @@ then
 	echo -e "${rojo_claro}ERROR:${amarillo} Debes ejecutar como ${cafe}sudo${no_color}"
 	salir 1
 fi
+# Guarda espacio libre antes de ejecución
+FREESPACE_BEFORE="$(df / | tail -1 | awk '{print $4}')"
+
 # Elimina cosas de apt-get
 echo -e "${amarillo}Borrando archivos de apt-get..."
 logWrite "Ejecutando funciones de apt-get"
@@ -31,9 +34,27 @@ logWrite "apt-get clean"
 apt-get clean 2>&1 /dev/null
 # Borrar logs antiguos
 echo -e "${amarillo}Borrando logs antiguos..."
-logWrite "Borrando logs comprimidos"
-rm /var/log/*.gz -r 2>&1 /dev/null
-logWrite "Borrando logs del estilo <nombre>.<número>"
-rm /var/log/*.{0..9} -r 2>&1 /dev/null
+logWrite "Borrando logs comprimidos, terminados con numero, terminados con .old..."
+find /var/log -regex "\(.*\.gz\'\|.*\.[0-9]+\'\\|.*\.old\'\)" | xargs rm
+#logWrite "Borrando logs comprimidos"
+#rm /var/log/*.gz -r 2>&1 /dev/null
+#logWrite "Borrando logs del estilo <nombre>.<número>"
+#rm /var/log/*.{0..9} -r 2>&1 /dev/null
+FREESPACE_AFTER="$(df / | tail -1 | awk '{print $4}')"
+FREED_SPACE="$(($FREESPACE_AFTER - $FREESPACE_BEFORE))"
+human() {
+	sufixes="BKMGTPE"
+	temp_size=$1
+	exp=0
+	factor=1
+	while [ "$temp_size" -ge "1024" ]; do
+		exp=$((exp+1))
+		factor=$((factor*1024))
+		temp_size="$(($1/$factor))"
+	done
+	echo "$temp_size""${sufixes:$exp:1}"
+}
+HUMAN_FREED_SPACE="$(human $FREED_SPACE)"
+echo -e "${azul_claro}Se liberaron $HUMAN_FREED_SPACE"
 echo -e "${verde_claro}Listo!${no_color}"
 salir 0
